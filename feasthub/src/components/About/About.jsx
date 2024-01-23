c601217441ab8ab6703a4d4c17db3bb9425e9817
Rohan Borade <rohanboradevit@gmail.com>
2024-01-23 21:57:02 +0530

    About Us

--- feasthub/src/components/About/About.jsx
+++ feasthub/src/components/About/About.jsx
@@ -21,20 +21,20 @@ const AboutUsPage = () => {
     <div className="Rectangle6 w-[450px] h-[408px] left-0 top-0 absolute bg-zinc-100" />
     <div className="Rectangle7 w-[450px] h-[408px] left-[482px] top-0 absolute bg-zinc-100" />
     <div className="Rectangle8 w-[450px] h-[408px] left-[964px] top-0 absolute bg-zinc-100" />
-    <div className="Review1 w-[265px] h-16 left-[88px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">Review 1</div>
-    <div className="Review2 w-[265px] h-16 left-[575px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">Review 2</div>
-    <div className="Review3 w-[265px] h-16 left-[1061px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">Review 3</div>
+    <div className="Review1 w-[265px] h-16 left-[88px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">★★★★★ Amazing Variety and Convenience!</div>
+    <div className="Review2 w-[265px] h-16 left-[575px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">★★★★★ <br />Best Blessing for Busy Lives!</div>
+    <div className="Review3 w-[265px] h-16 left-[1061px] top-[140px] absolute text-center text-black text-4xl font-normal font-['Yrsa']">★★★★★ Authentic Flavors, Every Time!</div>
   </div>
   <div className="ContactContent w-[1414px] h-[394px] left-[13px] top-[1474px] absolute">
     <div className="Rectangle9 w-[1414px] h-[394px] left-0 top-0 absolute bg-zinc-100" />
-    <div className="LoremIpsumDolorSitAmetConsecteturAdipiscingElitSedEuDuiHendreritEfficiturNequeInTempusElit w-[1058px] h-[127px] left-[178px] top-[134px] absolute text-center text-black text-2xl font-light font-['Merriweather']">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu dui hendrerit, efficitur neque in, tempus elit</div>
+    <div className="LoremIpsumDolorSitAmetConsecteturAdipiscingElitSedEuDuiHendreritEfficiturNequeInTempusElit w-[1058px] h-[127px] left-[178px] top-[134px] absolute text-center text-black text-2xl font-light font-['Merriweather']">Email: support@yourcompany.com <br />Phone: +91-XXXXXXXXXX (General inquiries) <br />Business Hours: Monday to Friday: 9:00 AM - 6:00 PM IST <br />Visit Us: FeastHub,Kotri Kalan, Ashta, Near, Indore Road, Bhopal, Madhya Pradesh 466114</div>
   </div>
   <div className="AboutUsContent w-[1072px] h-[267px] left-[184px] top-[367px] absolute">
-    <img className="Exclusive1 w-[183px] h-[173.02px] left-[447px] top-0 absolute" src="https://via.placeholder.com/183x173" />
-    <img className="Salad11 w-[183px] h-[162px] left-[25px] top-0 absolute" src="https://via.placeholder.com/183x162" />
+    <img className="Exclusive1 w-[183px] h-[173.02px] left-[447px] top-0 absolute" src="src/assets/About2.png" />
+    <img className="Salad11 w-[183px] h-[162px] left-[25px] top-0 absolute" src="src/assets/About1.png" alt='About1.png' />
     <div className="HomeMadeGoodness w-56 h-[61px] left-0 top-[193px] absolute text-center text-black text-2xl font-light font-['Merriweather']">Home made Goodness</div>
     <div className="ChooseFromTheBestTiffinServiceProviders w-[293px] h-[87px] left-[392px] top-[180px] absolute text-center text-black text-2xl font-light font-['Merriweather']">Choose from the best tiffin service providers</div>
-    <img className="RepairingService1 w-[183px] h-[162px] left-[869px] top-0 absolute" src="https://via.placeholder.com/183x162" />
+    <img className="RepairingService1 w-[183px] h-[162px] left-[869px] top-0 absolute" src="src/assets/About3.png" />
     <div className="CustomizableMealPlans w-[191px] h-[50px] left-[881px] top-[193px] absolute text-center text-black text-2xl font-light font-['Merriweather']">Customizable meal plans</div>
   </div>
   </>
--- feasthub/src/components/Header/Header.jsx
+++ feasthub/src/components/Header/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
   return (
     <>
     <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center text-4xl">
-                        <img src={logo} alt="Logo" className='h-10' />
-                    </Link>
-                    <div className="flex items-center lg:order-2">
+            <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="src/assets/logo.png" class="h-8" alt="FeastHub Logo" />
+                </a>
+                <div className="flex items-center lg:order-2">
                         {/* <Link
                             to="buyerlogin"
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
@@ -25,11 +25,14 @@ const Header = () => {
                             Log In / Register
                         </Link>
                     </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
                                 to="/"
@@ -83,9 +86,10 @@ const Header = () => {
                             
                             
                         </ul>
-                    </div>
                 </div>
+            </div>
             </nav>
+
         </header>
     </>
   )
