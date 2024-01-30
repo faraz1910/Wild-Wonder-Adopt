472864f4e487863f312aa190fae100c16ac06b37
faraz1910 <khalidfaraz63@gmail.com>
2024-01-30 23:07:38 +0530

    responsive navigation bar

--- feasthub/src/components/Header/Header.jsx
+++ feasthub/src/components/Header/Header.jsx
@@ -1,95 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "about" },
+    { name: "Why Us", link: "#" },
+    { name: "Order", link: "order" },
+    { name: "Contact", link: "contact" },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <header className="shadow sticky z-50 top-0">
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <Link to="/" className="flex items-center text-4xl">
+      <header className='shadow-md w-full fixed top-0 left-0 z-50'>
+        <nav className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+          <div
+            className="cursor-pointer flex items-center font-[Poppins] 
+      text-gray-800"
+          >
+            <Link to="/" className="text-3xl text-indigo-600 mr-1 pt-2">
               <img src={logo} alt="Logo" className="h-10" />
             </Link>
-            <div className="flex items-center lg:order-2">
-              {/*<Link
-                            to="buyerlogin"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log in
-  </Link>*/}
-                        <Link
-                            to="login"
-                            className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Log In / Register
-                        </Link>
-                    </div>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                    </svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <NavLink
-                                to="/"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-red-600' : 'text-gray-500'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0`
-                                    }
-                                >
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="/about"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-red-600' : 'text-gray-500'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0`
-                                    }
-                                >
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="#"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-red-600' : 'text-gray-500'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0`
-                                    }
-                                >
-                                    Why Us
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="order"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-red-600' : 'text-gray-500'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0`
-                                    }
-                                >
-                                    Order
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                to="#"
-                                    className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? 'text-red-600' : 'text-gray-500'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0`
-                                    }
-                                >
-                                    Contact Us
-                                </NavLink>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+          </div>
+
+          <div
+            onClick={() => setOpen(!open)}
+            className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+          >
+            <IoMenu />
+          </div>
+          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+            {Links.map((d, i) => (
+              <li>
+                <NavLink
+                  to={d.link}
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 ${
+                      isActive ? "text-red-600" : "text-gray-500"
+                    } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-700 lg:p-0 lg:mx-4 sm:m-0.5 md:m-0.5 md:text-base text-lg lg:font-semibold`
+                  }
+                >
+                  {d.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <Link
+            to="login"
+            className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none absolute top-6 right-16 md:static"
+          >
+            Log In / Register
+          </Link>
+        </nav>
+      </header>
+
+      
     </>
   );
 };
--- feasthub/tailwind.config.js
+++ feasthub/tailwind.config.js
@@ -1,16 +1,19 @@
+// tailwind.config.js
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
       backgroundImage: {
-        'homeimg1': "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0.8), rgba(0,0,0,0.5), transparent) ,url('/src/assets/homeimg1.jpg')",
-      }
+        homeimg1:
+          "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0.8), rgba(0,0,0,0.5), transparent) ,url('/src/assets/homeimg1.jpg')",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
