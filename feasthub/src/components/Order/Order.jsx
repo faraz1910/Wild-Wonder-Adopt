8c6eaaa4aa320891895a7f01f9a87f9f694fd2c2
aatmaja <aatmajapitale@gmail.com>
2024-03-17 17:12:23 +0530

    Set Pricing and display on order page

--- feasthub/src/components/Order/Order.jsx
+++ feasthub/src/components/Order/Order.jsx
@@ -57,10 +57,10 @@ const Order = () => {
                   <img src="src/assets/orderImg.jpg" alt="Seller" className="w-full h-full object-cover" />
                 </div>
                 <div className="p-4">
-                  <h4 className="font-bold text-lg mb-2">{b.businessName}</h4>
-                  <h5 className="font-bold text-lg mb-2">{b.fname} {b.lname}</h5>
+                  <h3 className="font-bold text-lg mb-2">{b.businessName}</h3>
+                  <h6 className="font-bold text-lg mb-1">₹ {b.price}</h6>
+                  <h5 className="text-base">{b.fname} {b.lname} - {b.phone}</h5>
                   <p className="mb-4">{b.address}</p>
-                  <p className="mb-4">{b.phone}</p>
                   <div className="flex justify-between items-center">
                     <div>
                     <span className="px-2 py-1 bg-pink-300 text-sm rounded mr-2">
