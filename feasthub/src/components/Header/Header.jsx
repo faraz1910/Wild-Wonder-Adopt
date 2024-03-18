337505cdd90d9800ff3fef7f7f06b3e270134e6c
faraz1910 <khalidfaraz63@gmail.com>
2024-03-18 10:37:51 +0530

    Dropdown for dashboard in navbar

--- feasthub/src/components/Header/Header.jsx
+++ feasthub/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import { IoMenu } from "react-icons/io5";
-import {auth} from "../../firebase";
+import { auth } from "../../firebase";
 import { secondaryAuth } from "../../firebaseSeller";
 
 const Header = () => {
@@ -73,13 +73,11 @@ const Header = () => {
         console.log("Error occurred during logout:", error);
       });
   };
-  
-  
 
   return (
     <>
-      <header className='shadow-md w-full sticky top-0 left-0 z-50'>
-        <nav className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+      <header className="shadow-md w-full sticky top-0 left-0 z-50">
+        <nav className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
           <div
             className="cursor-pointer flex items-center font-[Poppins] 
       text-gray-800"
@@ -95,7 +93,11 @@ const Header = () => {
           >
             <IoMenu />
           </div>
-          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+          <ul
+            className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+              open ? "top-20 " : "top-[-490px]"
+            }`}
+          >
             {Links.map((d, i) => (
               <li key={i}>
                 <NavLink
@@ -113,38 +115,54 @@ const Header = () => {
             ))}
           </ul>
           {sellerEmail ? (
-  <div className="flex items-center">
-    <span className="text-gray-700 mr-4 font-medium">
-      Logged in as: {sellerEmail}
-    </span>
-    <button
-      onClick={handleLogout}
-      className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-    >
-      Logout
-    </button>
-  </div>
-) : BuyerEmail ? (
-  <div className="flex items-center">
-    <span className="text-gray-700 mr-4 font-medium">
-      Logged in as: {BuyerEmail}
-    </span>
-    <Link to="/">
-    <button
-      onClick={handleLogoutBuyer}
-      className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-    >
-      Logout
-    </button></Link>
-  </div>
-) : (
-  <Link
-    to="login"
-    className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none absolute top-6 right-16 md:static"
-  >
-    Log In / Register
-  </Link>
-)}
+            <div className="flex items-center">
+              {/* <span className="text-gray-700 mr-4 font-medium">
+                Logged in as: {sellerEmail}
+              </span> */}
+              <button
+                onClick={handleLogout}
+                className=""
+              >
+                <div className="dropdown dropdown-hover ">
+                  <div tabIndex={0} role="button" className="btn">
+                  {sellerEmail}
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+                  >
+                    <li>
+                      <NavLink><a>Dashboard</a></NavLink>
+                    </li>
+                    <li>
+                      <NavLink>Logout</NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </button>
+            </div>
+          ) : BuyerEmail ? (
+            <div className="flex items-center">
+              <span className="text-gray-700 mr-4 font-medium">
+                Logged in as: {BuyerEmail}
+              </span>
+              <Link to="/">
+                <button
+                  onClick={handleLogoutBuyer}
+                  className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                >
+                  Logout
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <Link
+              to="login"
+              className="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none absolute top-6 right-16 md:static"
+            >
+              Log In / Register
+            </Link>
+          )}
         </nav>
       </header>
     </>
